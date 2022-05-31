12d4909b6b5c70947c5bf3b41d51b073a65ce046
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-05-30 20:26:28 -0500

    Add files via upload

---
+++ Phyton/1er parcial/Code4theWeek.18:11:2021.py
@@ -0,0 +1,11 @@
+#CODEFORTHEWEEK
+
+#circulo
+lado=int(input('medida del lado del circulo: '))
+
+print('el area del circulo es: ', (lado*lado))
+
+#trapezoide
+#triangulo1
+base=int(input('la base del triangulo 1 es: ')
+print('la base del triangulo es: ')
