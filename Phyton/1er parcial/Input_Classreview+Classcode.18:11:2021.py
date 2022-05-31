b564a7231b33075c7c383543a89f31f398116245
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-05-30 20:24:40 -0500

    Add files via upload

---
+++ Phyton/1er parcial/Input_Classreview+Classcode.18:11:2021.py
@@ -0,0 +1,66 @@
+#INPUT
+
+nombre=input('ingrese su nombre')
+print('saludos,',nombre)
+
+
+pais= input('de que pais sos vos?')
+print(pais,'apesta che!')
+
+print(nombre,'es de', pais, 'y es buena onda')
+
+#CLASSCODE
+
+#int sirve para para indicarle a input() que deve leer numeros
+
+numero=int(input('ingrese numero: '))
+numero2=int(input('ingrese numero: '))
+
+print('la suma de estos numeros es: ', (numero+numero2))
+
+#CLASSREVIEW
+#Instruccion: generar una calculadora que sume, reste, y divida, dos numeros enteros ingresados por el usuario
+
+#sumas
+numeroa=int(input('ingrese numero: '))
+numerob=int(input('ingrese numero: '))
+
+print('la suma de estos numeros es: ', (numeroa+numerob))
+
+numeroc=int(input('ingrese numero: '))
+numerod=int(input('ingrese numero: '))
+
+print('la suma de estos numeros es: ', (numeroc+numerod))
+
+#restas
+numeroa=int(input('ingrese numero: '))
+numerob=int(input('ingrese numero: '))
+
+print('la resta de estos numeros es: ', (numeroa-numerob))
+
+numeroc=int(input('ingrese numero: '))
+numerod=int(input('ingrese numero: '))
+
+print('la resta de estos numeros es: ', (numeroc-numerod))
+
+#multiplicaciones
+numeroa=int(input('ingrese numero: '))
+numerob=int(input('ingrese numero: '))
+
+print('la multiplicacion de estos numeros es: ', (numeroa*numerob))
+
+numeroc=int(input('ingrese numero: '))
+numerod=int(input('ingrese numero: '))
+
+print('la multiplicacion de estos numeros es: ', (numeroc*numerod))
+
+#diviciones
+numeroa=int(input('ingrese numero: '))
+numerob=int(input('ingrese numero: '))
+
+print('la division de estos numeros es: ', (numeroa/numerob))
+
+numeroc=int(input('ingrese numero: '))
+numerod=int(input('ingrese numero: '))
+
+print('la division de estos numeros es: ', (numeroc/numerod))
