d4a00ff96bc1c5a80fbf6a6693e73d0c20fc441c
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-05-30 20:01:48 -0500

    Add files via upload

---
+++ Phyton/1er parcial/21:oct:21.py
@@ -0,0 +1,19 @@
+print('how can we act with empaty and positivity when were online?')
+print('only commenting what you would say face to face')
+
+#feelings&options
+
+#identify
+print('The boy, the girl and the girls friend')
+print('the problem is that the girls fiend posted the letter')
+#feel
+print('at the begining the boy is exited for the answer, the girl thinks the letter is funny, the girls frien also feels like the letter is funny so she posts it, the boy feels sad after seeing that everyone is making fun of him and he felt even sadder after being expelled out of school')
+print('the boys situation is the hardest because he got expelled')
+#imagine
+print('if the boy wasnt crushing on her she wouldnt have sent the letter')
+print('if the girl havent sent the picture of the letter to her friend she wouldnt have posted it')
+print('if the girls friend wouldnt have posted the letters picture the whole schoole wouldnt have known about it')
+print('if all the school wouldnt have known about the letter the proffesor woldnt have known')
+print('if the proffesor wouldnt have known about the letter the guy wouldnt be expelled')
+#soy
+print('the boy woulnt have sent the letter, the girl wouldnt have sent the letter to her friend and maybe the girls friend would feel bad and wouldnt have posted the picture')
