239b2c5bee625e1f262274caf6096e11e38f1374
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-01 20:24:42 -0500

    Add files via upload

---
+++ Phyton/2do parcial/Classcode06:12:21.py
@@ -0,0 +1,19 @@
+comparador=1
+while(comparador<5):
+  name=input("Do you want a ticket? ")
+  print("here you go!")
+  comparador=comparador+1
+
+print("sorry, out of stock! come back later.")
+
+comparador=1
+while(comparador<=20):
+  name=input("enter your name: ")
+  comparador=comparador+1
+
+while(comparador<2):
+  name=input("Do you want a ticket? ")
+  print("here you go!")
+  comparador=comparador+1
+
+print("this machine has been hacked!")
