fd20097a5d8adc8be5e72f44b10e23e5b4da6a84
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-01 20:16:31 -0500

    Add files via upload

---
+++ Phyton/2do parcial/ClassReview23:11:2021.py
@@ -0,0 +1,20 @@
+#CLASSREVIEW
+edad=int(input('ingrese su edad: '))
+
+meses=int(edad*12)
+print('tu edad en meses es:', meses)
+
+semanas=int(edad*52)
+print('tu edad en semanas es:', semanas)
+
+dias=int(edad*365)
+print('tu edad en dias:', dias)
+
+horas=int(edad*24)
+print('tu edad en horas:', horas)
+
+minutos=int(edad*60)
+print('tu edad en minutos:', minutos)
+
+segundos=int(edad*60)
+print('tu edad en segundos:', segundos)
