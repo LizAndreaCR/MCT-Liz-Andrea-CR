56d1591e31d04780cfdd14c3e228d5e1a404a1d1
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-01 20:37:02 -0500

    Add files via upload

---
+++ Phyton/2do parcial/ClassCode03:01:22.py
@@ -0,0 +1,4 @@
+print("My Commitments For This Year Are:")
+print("Personal: Being more patient")
+print("School-related: Having better grades")
+print("Class-related: Paing attention so I understand the topics")
