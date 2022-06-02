e3fbc031a1c5d6ea90be765109727cbd990ac839
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-01 20:17:50 -0500

    Add files via upload

---
+++ Phyton/2do parcial/ClassCode29:11:21.py
@@ -0,0 +1,20 @@
+
+while True:
+  print('hola!, ¿Que haces?')
+  print('adios')
+  mensaje=input("contesta")
+  print('reprobados')
+
+#classreview: vuelva infinito el codigo que pregunta nombre, grado, grupo, etc..
+
+while True:
+print('como te llamas?')
+print('donde vives?')
+print('cual es tu color favorito?')
+print('cual es tu comida favorita?')
+print('cual es tu hobby?')
+print('que te apasiona?')
+print('a que lugares te gusta/gustaría ir?')
+print('te gustan los deportes?')
+print('cual es tu deporte preferido?')
+print('cual es tu animal favorito?')
