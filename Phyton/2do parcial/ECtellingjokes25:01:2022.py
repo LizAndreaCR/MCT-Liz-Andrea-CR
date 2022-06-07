42ba8392088d92324de6fed6a1ec927773ea0e3e
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-07 10:17:32 -0500

    Add files via upload

---
+++ Phyton/2do parcial/ECtellingjokes25:01:2022.py
@@ -0,0 +1,10 @@
+while True:
+  j=int(input('elija un numero del 1 al 5'))
+  if 1:
+    print('cual es la fruta que más ríe... la naranjajajaja')
+  if 2:
+    print('que le dice un techo a otro techo? te-cho de menos')
+  if 3:
+    print('el animal más viejo es el panda porque está en blanco y negro')
+  if 4:
+    print('como se maldicen entre pollos? caldito seas')
