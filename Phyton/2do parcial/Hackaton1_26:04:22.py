fd9f98db4acf4e4bb6c345d81846e283322491f1
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-07 10:29:29 -0500

    Add files via upload

---
+++ Phyton/2do parcial/Hackaton1_26:04:22.py
@@ -0,0 +1,11 @@
+print("CHATBOT")
+print("usted ha solicitado un ChatBot")
+print("Que necesita? Reportes de fallas, opcion a pago, promociones")
+
+options =input("Reportes de fallas, opcion a pago, promociones")
+
+if options == ("reportes de fallas") :
+   print("Cual es su reporte de fallas? TELEVISON o RED")
+other = input("Cual es su reporte de fallas? televison o red")
+if other == 'television': 
+print("este es tu numero de  reporte 64932")
