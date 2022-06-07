57a5119a3082e9c334cf0a7a75763b7ab7b56420
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-07 10:11:13 -0500

    Add files via upload

---
+++ Phyton/2do parcial/ClassCode01:02:2022.py
@@ -0,0 +1,31 @@
+variable=10
+
+if variable==10:
+  print("el numero es 10 y la primera condicion se cumple")
+
+elif variable==20:
+  print("el numero es 20 y el primer elif se cumple")
+
+elif variable==30:
+  print("el numero es 30 y el segundo elif se cumple")
+
+else:
+  print("el numero no es 10 y no se cumple la primera condicion")
+
+import datetime
+
+dia= datetime.date.today()
+w=dia.weekday()+1
+print(w)
+if (w==0):
+  print('feliz domingo!')
+elif (w==2)
+print('wuju! es martes')
+else:
+print('yay! es sabado')
+elif w==3
+print('ugh! es miercoles')
+elif w==4
+print('ush! es jueves')
+elif w==5
+print('por fin! ya es viernes')
