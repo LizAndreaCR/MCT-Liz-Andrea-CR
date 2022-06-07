3b7bb95b039fcb61bd8fb3f3881834d15fee8414
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-07 10:08:11 -0500

    Add files via upload

---
+++ Phyton/2do parcial/ClassCode18:Jan:22.py
@@ -0,0 +1,26 @@
+"""compara=35
+
+numero=int(input("ingresa un numero"))
+
+if(numero<35):
+print("te falta para adivinar")
+
+#NumeroEsElValorDeInicio
+
+if (numero>35):
+print("te sobra")
+
+#SirveParaTodoLoDemas
+
+else:
+print("lo lograste!")"""
+
+compara=35
+
+numero=int(input("ingresa un numero: "))
+
+if(numero==35):
+  print("lo lograste!")
+
+else:
+  print("fallaste:(")
