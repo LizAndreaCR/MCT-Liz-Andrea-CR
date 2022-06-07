9939416f445aa1738f960d6b0fcf48108502186b
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-07 10:16:04 -0500

    Add files via upload

---
+++ Phyton/2do parcial/ChallengeElif.py
@@ -0,0 +1,13 @@
+tiempo=int (input())
+while True:
+  if tiempo==25:
+     print("alerta critica")
+
+  elif tiempo>=20:
+    print("alerta rayo de cielo")
+
+elif tiempo==10:
+print("blah")
+
+else:
+print("nada que ver aquí")
