07add1662a99c9601439dede26788aae9e7a599e
LizAndreaCR <104395412+LizAndreaCR@users.noreply.github.com>
2022-06-07 10:21:13 -0500

    Add files via upload

---
+++ Phyton/2do parcial/ClassCode28:02:22.py
@@ -0,0 +1,9 @@
+while True:
+  x=int(input("escribe un numero"))
+  y=int(input("escribe un numero"))
+
+  if (x=1=6 and y==10):
+    print("se cumplen ambas condiciones")
+
+  if (x>0 or y<10):
+    print("se cumple una de las condiciones")
